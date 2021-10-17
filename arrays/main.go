084cdcc5e345347bbbe78d684e02652e6012d1cc
Marko <marko_poikkimaki@hotmail.com>
2021-10-17 15:08:33 +0200

    copy

--- arrays/main.go
+++ arrays/main.go
@@ -10,14 +10,26 @@ func main() {
 
 	cheeses = append(cheeses, "Camembert", "Hushållsost")
 
+	var copyCheeses = make([]string, 4)
+
+	copy(copyCheeses, cheeses)
+
 	// delete at index 2
 	// cheeses = append(cheeses[:2], cheeses[2+1:]...)
 
 	// delete at index 0
 	cheeses = append(cheeses[:0], cheeses[0+1:]...)
 
+	fmt.Println("CHEESES ****")
 	for i, s := range cheeses {
 		fmt.Println(i, s)
 	}
+
+	fmt.Println("COPY CHEESES ****")
+	for i, s := range copyCheeses {
+		fmt.Println(i, s)
+	}
+
+
 
 }
