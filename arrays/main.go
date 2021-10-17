76d88b297bcce4cbdb54b18929eb0252c99b0d73
Marko <marko_poikkimaki@hotmail.com>
2021-10-17 15:05:02 +0200

    delete

--- arrays/main.go
+++ arrays/main.go
@@ -8,7 +8,13 @@ func main() {
 	cheeses[0] = "Cheddar"
 	cheeses[1] = "Västerbotten"
 
-	cheeses = append(cheeses, "Camembert")
+	cheeses = append(cheeses, "Camembert", "Hushållsost")
+
+	// delete at index 2
+	// cheeses = append(cheeses[:2], cheeses[2+1:]...)
+
+	// delete at index 0
+	cheeses = append(cheeses[:0], cheeses[0+1:]...)
 
 	for i, s := range cheeses {
 		fmt.Println(i, s)
