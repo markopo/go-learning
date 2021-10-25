ce5ff5b91959bdff6eb85c55ad9e2f7ad4803f73
Marko <marko_poikkimaki@hotmail.com>
2021-10-25 19:04:30 +0200

    add batman

--- structs/main.go
+++ structs/main.go
@@ -9,11 +9,37 @@ type Movie struct {
 	Rating float32
 }
 
+type Superhero struct {
+	Name string
+	Age int
+	Address
+}
+
+type Address struct {
+	Number int
+	Street string
+	City string
+}
+
 func main() {
 	movie := Movie {
 		Name: "Blade Runner",
 		Rating: 10,
 	}
 
+	fmt.Println("%+v\n", movie)
+
 	fmt.Println(movie.Name, movie.Rating)
+
+	batman := Superhero{
+		Name: "Batman",
+		Age: 50,
+		Address: Address{
+			Number: 12,
+			Street: "Bat Cave",
+			City: "Gotham City",
+		},
+	}
+
+	fmt.Println("%+v\n", batman)
 }
