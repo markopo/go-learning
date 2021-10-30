8bc7b85930f2ccdcdf094941425512059a30f2ec
Marko <marko_poikkimaki@hotmail.com>
2021-10-30 12:56:04 +0200

    triangle

--- interfaces/main.go
+++ interfaces/main.go
@@ -29,6 +29,21 @@ func(s *Sphere) Volume() float64 {
 	return (float64(4) / float64(3)) * math.Pi * radiusCubed
 }
 
+type Triangle struct {
+	base float64
+	height float64
+}
+
+func(t *Triangle) area() float64 {
+	return 0.5 * (t.base * t.height)
+}
+
+func(t *Triangle) changeBase(f float64) {
+	t.base = f
+	return
+}
+
+
 
 
 
@@ -48,6 +63,17 @@ func main() {
 
 	fmt.Println(s.SurfaceArea())
 	fmt.Println(s.Volume())
+
+	t := Triangle{
+		base: 3,
+		height: 1,
+	}
+
+	fmt.Println(t.area())
+
+	t.changeBase(4)
+
+	fmt.Println(t.area())
 
 	fmt.Println("***** INTERFACES!!! *****")
 }
