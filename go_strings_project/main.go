ef86b42fb820c8f1c2ab3e66f37e618b747c5b06
Marko <marko_poikkimaki@hotmail.com>
2021-11-02 09:25:30 +0100

    string functions

--- go_strings_project/main.go
+++ go_strings_project/main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello")
 
 	var buffer bytes.Buffer
-	
+
 	for i := 0; i < 500; i++ {
 		buffer.WriteString("go! ")
 	}
 
-	fmt.Println(buffer.String())
+	str := buffer.String()
+
+	fmt.Println(str)
+
+	fmt.Println(strings.ToUpper(str))
+	fmt.Println(strings.ToLower(str))
+	fmt.Println(strings.Contains(str, "go!"))
+	fmt.Println(strings.Repeat("Hello! \n", 10))
 }
