1b9d2e9eb595811082bec976e3aaabad3c552081
Marko <marko_poikkimaki@hotmail.com>
2021-11-14 12:13:39 +0100

    make concurrent http requests

--- go_routines_project/main.go
+++ go_routines_project/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -10,10 +12,39 @@ func slowFunc() {
 	fmt.Println("sleeper finished..")
 }
 
+func responseTime(url string)  {
+	start := time.Now()
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	elapsed := time.Since(start).Seconds()
+
+	fmt.Println("‰s took %v seconds.", url, elapsed)
+}
+
 func main() {
-	go slowFunc()
+	//go slowFunc()
+	//
+	//fmt.Println("All finished! BINGO!")
+	//
+	//time.Sleep(time.Second * 3)
 
-	fmt.Println("All finished! BINGO!")
+	urls := make([]string, 5)
+	urls[0] = "https://sr.se"
+	urls[1] = "https://svt.se"
+	urls[2] = "https://dn.se"
+	urls[3] = "https://www.usa.gov"
+	urls[4] = "https://expressen.se"
 
-	time.Sleep(time.Second * 3)
+	for _, u := range urls {
+		go responseTime(u)
+	}
+
+	time.Sleep(time.Second * 5)
 }
