2ea0cb1c4c91eacc883c8cd215f2ee022cbdd1e3
Marko <marko_poikkimaki@hotmail.com>
2021-12-07 07:48:48 +0100

    add divide

--- webProjectOne/main.go
+++ webProjectOne/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -24,11 +25,35 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	x := 100.0
+	y := 0.0
+	sum, err := divideValues(float32(x), float32(y))
+
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("Divide %f / %f = %f", x, y, sum))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Can't divide by zero!")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting the app at port%s", portNumber))
 
