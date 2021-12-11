a4fef2b8464e195394d10cc3ceb402965c26b2a9
Marko <marko_poikkimaki@hotmail.com>
2021-12-11 17:15:01 +0100

    create layout

--- webProjectOne/pkg/render/render.go
+++ webProjectOne/pkg/render/render.go
@@ -1,17 +1,75 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
 	"html/template"
+	"path/filepath"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-err := parsedTemplate.Execute(w, nil)
+var functions = template.FuncMap{}
 
-if err != nil {
-fmt.Println("error parsing template: ", err)
-return
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	tc, err := CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, ok := tc[tmpl]
+
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+    _, err = buf.WriteTo(w)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page: ", page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err := ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+			myCache[name] = ts
+		}
+
+
+	}
+
+	return myCache, nil
 }
