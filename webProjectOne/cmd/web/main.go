1dc7eca5413afafff6d9b4aceba22b0a31b71829
Marko <marko_poikkimaki@hotmail.com>
2021-12-11 16:12:55 +0100

    refactor app

--- webProjectOne/cmd/web/main.go
+++ webProjectOne/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"github.com/markopo/go-learning/pkg/handlers"
 )
 
 const portNumber = ":6969"
@@ -14,8 +15,8 @@ const portNumber = ":6969"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting the app at port%s", portNumber))
 
--- webProjectOne/pkg/handlers/handlers.go
+++ webProjectOne/pkg/handlers/handlers.go
@@ -1,17 +1,18 @@
-package main
+package handlers
 
 import (
+	"github.com/markopo/go-learning/pkg/render"
 	"net/http"
 )
 
 // Home Page
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About Page
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
 
 
