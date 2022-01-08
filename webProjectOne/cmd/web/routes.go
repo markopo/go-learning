6e394f251b2e2059e34bb94a48b415440b124c48
Marko <marko_poikkimaki@hotmail.com>
2022-01-08 15:47:35 +0100

    use middleware.Recoverer

--- webProjectOne/cmd/web/routes.go
+++ webProjectOne/cmd/web/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/markopo/go-learning/pkg/config"
 	"github.com/markopo/go-learning/pkg/handlers"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 func routes(app *config.AppConfig) http.Handler  {
 	mux := chi.NewRouter()
 
-
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
