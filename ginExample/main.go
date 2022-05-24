d41c36a48de1bf65052aea642d758183b2839e74
Marko <marko_poikkimaki@hotmail.com>
2022-05-24 20:55:40 +0200

    Add gin gonic example

---
+++ ginExample/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+type Person struct {
+	Name string `form:"name"`
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(context *gin.Context) {
+
+		var person Person
+		name := ""
+
+		if context.ShouldBind(&person) == nil {
+			name = "God day " + person.Name
+		}
+
+		context.JSON(200, gin.H{
+			"message": "Hello from Gin! " + name + "!",
+		})
+	})
+
+	r.GET("/ping-time", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"serverTime": time.Now().UTC(),
+		})
+	})
+
+	r.Run(":6868")
+}
